0a9b16ad7fefda3710aba958651ed6abafdab1cc
jiro <jiro853141@gmail.com>
2021-12-14 22:23:34 +0800

    feat: unplugin-icons - Heroicons 介紹

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,4 +1,16 @@
+<script>
+// import IconHome from '~icons/heroicons-outline/home'
+
+// export default {
+//   components: {
+//     IconHome,
+//   },
+// }
+</script>
+
 <template>
   <div>Home</div>
-  <HelloWorld msg="Hello Horld" />
+  <!-- <IconHome class="w-8 h-8 text-violet-600" /> -->
+  <!-- <HeroiconsOutlineHome class="w-8 h-8 text-violet-600" /> -->
+  <heroicons-outline-user class="w-16 h-16 text-red-600" />
 </template>
