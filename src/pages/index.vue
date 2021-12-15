09f6b5fd6b0ea82b546c90a82707d844b14709f1
jiro <jiro853141@gmail.com>
2021-12-15 23:31:35 +0800

    feat: 頁面布局 - 桌機版

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,16 +1,3 @@
-<script>
-// import IconHome from '~icons/heroicons-outline/home'
-
-// export default {
-//   components: {
-//     IconHome,
-//   },
-// }
-</script>
-
 <template>
-  <div>Home</div>
-  <!-- <IconHome class="w-8 h-8 text-violet-600" /> -->
-  <!-- <HeroiconsOutlineHome class="w-8 h-8 text-violet-600" /> -->
-  <heroicons-outline-user class="w-16 h-16 text-red-600" />
+  <Layout></Layout>
 </template>
