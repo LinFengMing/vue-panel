1c7bb5960686ce9cc1f30c693fdb09974d7ba45e
jiro <jiro853141@gmail.com>
2021-12-17 00:39:01 +0800

    feat: 頁面布局 - 手機版

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,47 +1,83 @@
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const showMenu = ref(false)
+    const toggleMenu = () => {
+      showMenu.value = !showMenu.value
+    }
+
+    return { showMenu, toggleMenu }
+  }
+}
+</script>
+
 <template>
-  <div class="flex min-h-screen">
+  <div class="flex flex-col sm:flex-row min-h-screen">
     <!-- 左側選單欄 -->
-    <div class="flex flex-col justify-between w-64 bg-violet-600 text-white">
-      <div>
-        <!-- Logo -->
-        <div class="flex justify-center items-center h-16">
+    <div
+      class="flex flex-col flex-shrink-0 justify-between w-full bg-violet-600 text-white sm:w-64"
+    >
+      <!-- Logo -->
+      <div class="flex justify-between items-center h-16 px-4 sm:justify-center">
+        <div class="flex justify-center items-center">
           <heroicons-outline-book-open class="w-10 h-10 mr-2" />
           <span class="text-xl font-medium">Press</span>
         </div>
+
+        <button class="sm:hidden flex justify-center items-center w-8 h-8" @click="toggleMenu">
+          <heroicons-outline-menu class="w-5 h-5" />
+        </button>
+      </div>
+
+      <div class="sm:flex flex-grow flex-col justify-between" :class="showMenu ? '' : 'hidden'">
         <!-- 主要選單 -->
         <ul>
           <li>
-            <RouterLink to="/" class="flex items-center px-5 py-3 text-white">
+            <RouterLink to="/" class="flex items-center px-5 py-4 sm:px-5 text-white">
               <heroicons-outline-home class="w-5 h-5 mr-2" />首頁
             </RouterLink>
           </li>
           <li>
-            <RouterLink to="/" class="flex items-center px-5 py-3 text-violet-400 hover:text-white">
+            <RouterLink to="/" class="flex items-center px-5 py-4 text-violet-400 hover:text-white">
               <heroicons-outline-document-text class="w-5 h-5 mr-2" />文章
             </RouterLink>
           </li>
           <li>
-            <RouterLink to="/" class="flex items-center px-5 py-3 text-violet-400 hover:text-white">
+            <RouterLink to="/" class="flex items-center px-5 py-4 text-violet-400 hover:text-white">
               <heroicons-outline-user class="w-5 h-5 mr-2" />個人資料
             </RouterLink>
           </li>
         </ul>
-      </div>
-      <!-- 用戶名稱 -->
-      <div class="flex justify-between items-center px-5 py-4 border-t border-violet-400">
-        <div class="flex items-center">
-          <img
-            class="w-8 h-8 rounded-full mr-2"
-            src="https://cdn.jsdelivr.net/npm/slidev-theme-ycs77/public/images/lucas_v_avatar.jpg"
-          />
-          <div class="font-medium tracking-wide">Jiro</div>
+        <!-- 用戶名稱 手機版 -->
+        <div class="sm:hidden mt-2 py-1 border-t border-violet-400">
+          <div class="flex items-center px-4 py-3">
+            <img
+              class="w-8 h-8 rounded-full mr-2"
+              src="https://cdn.jsdelivr.net/npm/slidev-theme-ycs77/public/images/lucas_v_avatar.jpg"
+            />
+            <div class="font-medium tracking-wide">Jiro</div>
+          </div>
         </div>
-
-        <button
-          class="flex justify-center items-center w-7 h-7 hover:bg-violet-400 rounded transition-colors duration-100"
+        <!-- 用戶名稱 電腦版 -->
+        <div
+          class="hidden sm:flex sm:justify-between sm:items-center px-5 py-4 border-t border-violet-400"
         >
-          <heroicons-outline-dots-vertical class="w-4 h-4" />
-        </button>
+          <div class="flex items-center">
+            <img
+              class="w-8 h-8 rounded-full mr-2"
+              src="https://cdn.jsdelivr.net/npm/slidev-theme-ycs77/public/images/lucas_v_avatar.jpg"
+            />
+            <div class="font-medium tracking-wide">Jiro</div>
+          </div>
+
+          <button
+            class="flex justify-center items-center w-7 h-7 hover:bg-violet-400 rounded transition-colors duration-100"
+          >
+            <heroicons-outline-dots-vertical class="w-4 h-4" />
+          </button>
+        </div>
       </div>
     </div>
     <!-- 右側內容區 -->
@@ -50,7 +86,3 @@
     </div>
   </div>
 </template>
-<script>
-export default {
-}
-</script>
