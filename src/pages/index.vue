fb90da68d479994e68ced590db82dfc616254737
jiro <jiro853141@gmail.com>
2021-12-25 01:37:29 +0800

    feat: 訊息提示組件 - 功能

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,15 +1,10 @@
 <script>
-import { notify } from '@kyvg/vue3-notification';
+import { warningNotify } from '@/composables/useNotification';
 
 export default {
   setup() {
     const open = () => {
-      notify({
-        // type: 'success',
-        title: '更新成功',
-        // text: '已經更新成功了',
-        duration: 1000000000,
-      });
+      warningNotify('更新成功')
     }
 
     return { open }
